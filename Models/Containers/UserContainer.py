320dc435c4a7a5d5d4aec5862ee2d35b8d53c9f8
Ruslan Radetskiy <russ2510@gmail.com>
2019-05-18 14:22:49 -0400

    creating a test suit to test userContainer

--- Models/Containers/UserContainer.py
+++ Models/Containers/UserContainer.py
@@ -9,7 +9,9 @@ import Models.SQLModels.Subreddit as SR
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+#every instance of this class should be its own thread
 class UserContainer:
+    ##session need
     @staticmethod
     def startSession():
         engine = create_engine('mysql+pymysql://root:Pen1992@@localhost/redditStalker?charset=utf8', encoding='utf-8')
@@ -23,11 +25,37 @@ class UserContainer:
 
 
     def insertSubreddit(self, subredditName):
+        session = UserContainer.startSession()
 
         #check if subreddit entry exists
-        #link user and subreddit
-        #save in table
-        pass
+        checkSubreddit = session.query(Sub.Subreddit).filter_by(name = subredditName).first()
+
+        # link user and subreddit
+        if checkSubreddit != None:
+            new_subreddit =  Sub.Subreddit(
+                name = subredditName
+            )
+            session.add(new_subreddit)
+
+            new_subreddit_user = Sub.SubredditUserJoin(
+                user = self.user,
+                subredditName = new_subreddit
+
+            )
+
+            session.add(new_subreddit_user)
+            session.commit()
+        else:
+
+            new_subreddit_user = Sub.SubredditUserJoin(
+                user=self.user,
+                subredditName=checkSubreddit
+
+            )
+
+            session.add(new_subreddit_user)
+            session.commit()
+
 
     def run(self):
         #thread runner class...
